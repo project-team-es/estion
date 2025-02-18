1c93d4e21183f674e9fb0ad19974ad22c0cb2fb5
rrr-1o23 <im23d067@gmail.com>
2025-02-18 12:34:25 +0900

    fix: dashboard画面で締め切りが過ぎたESは非表示されるように修正

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -18,10 +18,13 @@ class DashboardController extends Controller implements HasMiddleware
         $bookmarks = $user->bookmark()->get();
         // ログインユーザーに紐づく業界のみ取得
         $industries = $user->industries()->get();
+        // 締切間近ES取得
+        // 締切間近ES取得（過去の日付を除外）
         $entrysheets = $user->entrysheet()
-            ->orderByRaw('ISNULL(deadline), deadline ASC')
-            ->get();
-    
+        ->where('deadline', '>=', now()) // 現在の日時より未来のもののみ取得
+        ->orderByRaw('ISNULL(deadline), deadline ASC')
+        ->get();
+            
         // ユーザーに紐づく企業のみを取得する
         $industriesWithCompanies = $industries->mapWithKeys(function ($industry) use ($user) {
             return [
