31ac9109e9bce389ceb096ee0f9cc19b3c2cf07d
slopebook1219 <saka.ttt.1219@gmail.com>
2025-02-21 22:20:53 +0900

    検索アイコンを追加

--- resources/views/entrysheet/home.blade.php
+++ resources/views/entrysheet/home.blade.php
@@ -14,8 +14,9 @@
 
                         <!-- フィルターボタン（右端） -->
                         <button id="filterButton" 
-                                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-[12px]">
-                            フィルターをかける
+                            class="text-gray-500 font-bold py-2 px-4 rounded-full transition
+                                hover:bg-gray-300 hover:text-white">
+                            {!! config('icons.search') !!}
                         </button>
                     </div>
 
