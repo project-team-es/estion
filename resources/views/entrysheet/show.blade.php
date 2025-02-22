1ab9d9f2025b1098ddde736ae5329e089b28ce1b
rrr-1o23 <im23d067@gmail.com>
2025-02-22 22:51:04 +0900

    fix: entrysheet.show_概要編集ボタンをアイコンに変更

--- resources/views/entrysheet/show.blade.php
+++ resources/views/entrysheet/show.blade.php
@@ -15,10 +15,14 @@
                                class="bg-blue-600 text-white px-3 py-1 rounded-full text-sm hover:bg-blue-500">
                                 PDF
                             </a>
-                            
                         </div>
-                        <a href="{{ route('entrysheet.edit', $entrysheet) }}" class="bg-blue-500 text-white px-4 py-2 rounded-[12px]">
-                            編集
+                        
+                        <a href="{{ route('entrysheet.edit', $entrysheet) }}"
+                            class="inline-flex items-center justify-center p-2 rounded-full hover:bg-gray-200 transition-colors duration-200 hover:cursor-pointer">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" style="opacity: 0.5;" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pencil">
+                                <path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z"/>
+                                <path d="m15 5 4 4"/>
+                            </svg>
                         </a>
                     </div>
 
