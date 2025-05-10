c98012cf19dc7e3ea0db4bc26ee32c127c0c4138
坂本大幹 <sakamototaiki@sakamototaikannoMacBook-Air.local>
2025-05-10 10:49:25 +0900

    お問い合わせフォームの作成。ドロップダウンにもお問い合わせリンクを追加

--- resources/js/Pages/Guest/components/NavBar/NavLinks/index.jsx
+++ resources/js/Pages/Guest/components/NavBar/NavLinks/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function NavLinks({ auth }) {
     return (
         <div className="hidden md:flex space-x-10 font-bold">
-            <a href="/contact" className="text-black hover:text-gray-300 transition">
+            <a href="https://docs.google.com/forms/d/1TIZ2sr5REvMeDEHOZPBY9imnWFb21UEDT2sUpRVtZAo/edit#settings" className="text-black hover:text-gray-300 transition">
                 お問い合わせ
             </a>
             {auth?.user ? (
