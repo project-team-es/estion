b2e5bbc417003d68120056e3cb4c5a003326092a
rrr-1o23 <im23d067@gmail.com>
2025-05-25 15:22:53 +0900

    test

--- app/Models/EntrySheet.php
+++ app/Models/EntrySheet.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class EntrySheets extends Model
+class EntrySheet extends Model
 {
     use HasFactory, SoftDeletes;
 
