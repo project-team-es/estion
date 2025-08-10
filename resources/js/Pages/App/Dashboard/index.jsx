b2fd3639d3eaa6f2e14f1d8673b154ac87d6d76c
rrr-1o23 <im23d067@gmail.com>
2025-08-10 16:43:31 +0900

    feat: content-listの部分スクロールを実装

--- resources/js/Pages/App/Dashboard/index.jsx
+++ resources/js/Pages/App/Dashboard/index.jsx
@@ -6,13 +6,13 @@ import AppLayout from '@/Layouts/AppLayout';
 export default function Dashboard({ industries, contents, entrysheets, industriesWithCompanies }) {
   return (
     <AppLayout title="ホーム">
-      <div className="flex min-h-screen md:mt-10">
+      <div className="flex h-screen md:mt-10">
         <IndustryList
           className="z-10 flex h-screen w-1/5 flex-col justify-between"
           industries={industries}
           industriesWithCompanies={industriesWithCompanies}
         />
-        <main className="ml-[1%] mr-[1%] flex-grow px-4">
+        <main className="ml-[1%] mr-[1%] flex-grow h-full px-4 overflow-y-auto">
           <ContentList contents={contents} />
         </main>
         {entrysheets && (
