2abf9c1f518ecc2acac4361615fe1c7b3c9de98d
rrr-1o23 <im23d067@gmail.com>
2025-08-18 12:33:16 +0900

    feat: Q&A周りのtitleとmetaタグを追加

--- resources/js/Pages/App/Qa/index.jsx
+++ resources/js/Pages/App/Qa/index.jsx
@@ -1,6 +1,16 @@
+import { Head, Link } from '@inertiajs/react';
+
 export default function Agreement() {
   return (
     <>
+      <Head>
+        <title>Q&A</title>
+        <meta
+          name="description"
+          content="estion.のQ&Aページです。estion.の使い方やよくある質問を確認することができます。"
+        />
+      </Head>
+
       <p>QAです</p>
     </>
   );
