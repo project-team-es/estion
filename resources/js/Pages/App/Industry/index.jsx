ce35312ec78d22f0b7760aeddd4eaa1609860d63
rrr-1o23 <im23d067@gmail.com>
2025-08-18 12:19:57 +0900

    feat: 業界ページのtitleとmetaタグを修正及び追加

--- resources/js/Pages/App/Industry/index.jsx
+++ resources/js/Pages/App/Industry/index.jsx
@@ -1,9 +1,17 @@
 import AppLayout from '@/Layouts/AppLayout';
 import IndustryList from './IndustryList';
+import { Head } from '@inertiajs/react';
 
 export default function index({ industries, industriesWithCompanies }) {
   return (
-    <AppLayout title="企業一覧">
+    <AppLayout>
+      <Head>
+        <title>業界一覧</title>
+        <meta
+          name="description"
+          content="estion.の業界一覧ページです。業界ごとに企業を確認することができます。"
+        />
+      </Head>
       <div className="pt-10">
         <IndustryList industries={industries} industriesWithCompanies={industriesWithCompanies} />
       </div>
