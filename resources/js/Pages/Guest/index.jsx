63a755beba818c79532b2800c155b458587b5138
rrr-1o23 <im23d067@gmail.com>
2025-09-23 17:27:54 +0900

    feat: estion.についてに関するカードスタイルを追加

--- resources/js/Pages/Guest/index.jsx
+++ resources/js/Pages/Guest/index.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import NavBar from '@/Pages/Guest/components/NavBar';
+import CardSection from '@/Pages/Guest/components/CardSection';
 import { Head } from '@inertiajs/react';
 import { useState } from 'react';
 import FooterForGueset from '@/Components/FooterForGuset';
+
 
 export default function Guest() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const toggleMenu = () => {
     setIsMenuOpen((prev) => !prev);
   };
+
   return (
     <>
       <Head>
@@ -17,11 +20,11 @@ export default function Guest() {
           name="description"
           content="新卒就活生向けのエントリーシート（ES）管理アプリestion.(イーション)です。estion.を活用して多種多様な業界・企業のESを効率的に管理しましょう!"
         />
-        <meta name="google-adsense-account" content="ca-pub-9604843985307640"/>
+        <meta name="google-adsense-account" content="ca-pub-9604843985307640" />
       </Head>
 
       <div
-        className={`flex min-h-screen flex-col bg-cover bg-center bg-no-repeat ${isMenuOpen ? 'overflow-hidden' : ''}`}
+        className={`relative flex min-h-screen flex-col bg-cover bg-center bg-no-repeat after:absolute after:bottom-0 after:left-0 after:h-[10%] after:w-full after:bg-gradient-to-t after:from-white after:to-transparent after:backdrop-blur-sm after:content-[''] ${isMenuOpen ? 'overflow-hidden' : ''}`}
         style={{ backgroundImage: "url('/image/front/paper.png')" }}
       >
         <NavBar isMenuOpen={isMenuOpen} toggleMenu={toggleMenu} />
@@ -38,8 +41,11 @@ export default function Guest() {
             </p>
           </div>
         </div>
-        <FooterForGueset />
       </div>
+
+      <CardSection />
+
+      <FooterForGueset />
     </>
   );
 }
